6a5d85659a3c68097dcb8fe975250b803991cb18
murodin <murat_dindar@yahoo.com>
2020-05-24 17:59:21 +0300

    Lets start

---
+++ src/main/java/tug/it/openprojectapi/OpenprojectApiApplication.java
@@ -0,0 +1,13 @@
+package tug.it.openprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenprojectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenprojectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tug/it/openprojectapi/OpenprojectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tug.it.openprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenprojectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
